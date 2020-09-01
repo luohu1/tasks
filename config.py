da9e951447f110de49eb50691ef0a670a7b0fcd4
luohu1 <mylhmail@163.com>
2020-09-01 21:41:22 +0800

    feat: Using the filesystem to store task state and results.

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
-## Broker settings.
-broker_url = 'redis://dev:6380/6'
+from pathlib import Path
+
+_data = Path(__file__).cwd().joinpath('data')
+_backend_folder = _data.joinpath('results')
+_backend_folder.mkdir(parents=True, exist_ok=True)
+
+# Broker settings.
+broker_url = 'redis://127.0.0.1:6379/6'
 
 # List of modules to import when the Celery worker starts.
 imports = ('tasks', )
+
+# Using the filesystem to store task state and results.
+result_backend = _backend_folder.as_uri()
